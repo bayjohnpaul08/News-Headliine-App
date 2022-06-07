debf02634bc8cc04f28834b39975880d83622f6b
bayjohnpaul08 <bay.johnpaul08@gmail.com>
2022-06-07 23:46:39 +0800

    first commit

---
+++ app/src/main/java/com/example/newsapp/domain/repository/NewsRepository.kt
@@ -0,0 +1,18 @@
+package com.example.newsapp.domain.repository
+
+import com.example.newsapp.data.model.APIResponse
+import com.example.newsapp.data.model.Article
+import com.example.newsapp.data.util.Resource
+import kotlinx.coroutines.flow.Flow
+
+/**
+*  Repository that communicate to local database and remote database
+* */
+interface NewsRepository {
+
+    suspend fun getNewsHeadline(): Resource<APIResponse> // get news to remote database
+    suspend fun getSearchedNews(searchQuery: String): Resource<APIResponse> // search news to remote database
+    suspend fun saveNews(article: Article) // save news to local database
+    suspend fun deleteNews(article: Article) // delete new to local database
+    fun getSavesNews(): Flow<List<Article>> // get saved new to local database
+}
